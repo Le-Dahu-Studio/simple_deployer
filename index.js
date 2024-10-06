37761339c067cd2031df1cb7b1a2bfb337a11e21
Matt <torsello.mattia@gmail.com>
2024-10-06 16:56:07 +0200

    changed port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const shell = require('shelljs')
 const app = express()
-const port = 3000
+const port = 3030
 
 app.get('/', (req, res) => {
     shell.exec("sh test.sh")
